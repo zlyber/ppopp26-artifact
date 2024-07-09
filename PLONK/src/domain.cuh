2c0057f4a9b0b024def612a53aab3ff0d325a05a
zlyber <496035867@qq.com>
2024-07-09 12:40:06 +0800

    add domain.cuh

---
+++ PLONK/src/domain.cuh
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <cstdlib>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include "PLONK/src/bls12_381/fr.hpp"
+#include "PLONK/utils/function.cuh"
+
+class Radix2EvaluationDomain{
+    public:
+        int size;
+        int log_size_of_group;
+        SyncedMemory& size_as_field_element;
+        SyncedMemory& size_inv;
+        SyncedMemory& group_gen;
+        SyncedMemory& group_gen_inv;
+        SyncedMemory& generator_inv;
+
+        Radix2EvaluationDomain(int num_coeffs);
+        SyncedMemory& get_root_of_unity(int n);
+        SyncedMemory& evaluate_all_lagrange_coefficients(SyncedMemory& tau);
+        SyncedMemory& evaluate_vanishing_polynomial(SyncedMemory& tau);
+        SyncedMemory& element(int i);
+};
